d477318c6621e441d02eb2cb1ae8b78c5d649709
JasmineJoves <mine.obra11@gmail.com>
2024-06-08 17:51:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const btnLike2 = document.getElementById("btnLike2")
 const countLike1 = document.getElementById("countLike1")
 const countLike2 = document.getElementById("countLike2")
 
-function hitLike1() {
+function clickLike1() {
     let totalLike1 = parseInt(countLike1.value) + 1
     countLike1.textContent = totalLike1.toString()
 } 
-function hitLike2() {
+function clickLike2() {
     let totalLike2 = parseInt(countLike2.value) + 1
     countLike2.textContent = totalLike2.toString()
 } 
-btnLike1.addEventListener("click", hitLike1)
-btnLike2.addEventListener("click", hitLike2)
+btnLike1.addEventListener("click", clickLike1)
+btnLike2.addEventListener("click", clickLike2)
 
-const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
+const submit = document.getElementById("submit")
 const commentbox = document.getElementById("commentbox")
-function submitComment() {
+function submitComment(){
     commentbox.textContent += comment.value.toString() + "\n"
 } 
-submit.addEventListener("click", submitComment)
+submit.addEventListener("click",submitComment)
